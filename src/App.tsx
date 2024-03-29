c61b63629525a7c6c99e872a567b44eb38ad1e83
AndriySt92 <andriyua-1992@mail.ru>
2024-03-29 12:44:12 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react"
+
+const App = () => {
+  return (
+    <div>
+      <Button color="primary">Button</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
