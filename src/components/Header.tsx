717930e1bde458d30bd3f45deabf62cd67198d88
AndriySt92 <andriyua-1992@mail.ru>
2024-04-02 19:46:42 +0300

    Fixed Header component -  added logout feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,28 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  Button,
 } from "@nextui-org/react"
 import { LuSunMedium } from "react-icons/lu"
 import { FaRegMoon } from "react-icons/fa"
 import { useContext } from "react"
 import { ThemeContext } from "./ThemeProvider"
+import { useAppDispatch, useAppSelector } from "../app/hooks"
+import { logout, selectIsAuthenticated } from "../features/user/userSlice"
+import { useNavigate } from "react-router-dom"
+import { CiLogout } from "react-icons/ci"
 
 export const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext)
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    localStorage.removeItem("token")
+    navigate("/auth")
+  }
 
   return (
     <Navbar>
@@ -25,7 +39,18 @@ export const Header = () => {
         >
           {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
         </NavbarItem>
-        <NavbarItem></NavbarItem>
+        <NavbarItem>
+          {isAuthenticated && (
+            <Button
+              color="default"
+              variant="flat"
+              className="gap-2"
+              onClick={handleLogout}
+            >
+              <CiLogout /> <span>Выйти</span>
+            </Button>
+          )}
+        </NavbarItem>
       </NavbarContent>
     </Navbar>
   )
