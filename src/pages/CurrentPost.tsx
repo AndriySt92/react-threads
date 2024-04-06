a1f92b56b289038ba70901fdf53ffc0f10ed2475
AndriySt92 <andriyua-1992@mail.ru>
2024-04-06 15:30:41 +0300

    Added CurrentPost page

--- src/pages/CurrentPost.tsx
+++ src/pages/CurrentPost.tsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useGetPostByIdQuery } from '../app/services/postApi'
+import { Card, CreateComment, GoBack } from '../components'
 
 export const CurrentPost = () => {
+  const params = useParams<{ id: string }>()
+  const { data } = useGetPostByIdQuery(params?.id ?? "")
+
+  if (!data) {
+    return <h2>Post doesn't exist</h2>
+  }
+
+  const {
+    content,
+    id,
+    authorId,
+    comments,
+    likes,
+    author,
+    likedByUser,
+    createdAt,
+  } = data
+
   return (
-    <div>CurrentPost</div>
+    <>
+      <GoBack />
+      <Card
+        cardFor="current-post"
+        avatarUrl={author?.avatarUrl ?? ""}
+        content={content}
+        name={author?.name ?? ""}
+        likesCount={likes.length}
+        commentsCount={comments?.length}
+        authorId={authorId}
+        id={id}
+        likedByUser={likedByUser}
+        createdAt={createdAt}
+      />
+      <div className="mt-10">
+        <CreateComment />
+      </div>
+      <div className="mt-10">
+        {data.comments
+          ? data.comments.map((comment) => (
+              <Card
+                cardFor="comment"
+                key={comment.id}
+                avatarUrl={comment.user.avatarUrl ?? ""}
+                content={comment.content}
+                name={comment.user.name ?? ""}
+                authorId={comment.userId}
+                commentId={comment.id}
+                id={id}
+              />
+            ))
+          : null}
+      </div>
+    </>
   )
 }
